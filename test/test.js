9c3a0ac82376e46f8720e455f8474de01581fae7
Aziz Khoury <bentael@gmail.com>
2016-01-07 02:23:00 -0500

    using newer version of bbcodejs

--- test/test.js
+++ test/test.js
@@ -2,15 +2,15 @@ var convert = require('../src/bbcode-to-markdown');
 
 var samples = require('./samples.json');
 
-var assert = function (a, b, sample) {
+var assert = function (a, b, sample, i) {
 	if (a != b) {
-		console.log(a, "!=", b);
-		throw JSON.stringify(sample, null, 2) + ", Failed";
+		console.log("'" + a + "'", "!=", b);
+		throw JSON.stringify(sample, null, 2) + ", Failed, " + i;
 	}
 };
 
-samples.forEach(function(sample) {
-	assert(convert.bbcodeToHTML(sample.raw), sample.html, sample);
+samples.forEach(function(sample, i) {
+	assert(convert.bbcodeToHTML(sample.raw), sample.html, sample, i);
 	assert(convert.bbcodeToMarkdown(sample.raw), sample.md, sample);
 });
 console.log("All good, " + samples.length + " passed.");
