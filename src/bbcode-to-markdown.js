041e3ad69eab0781a35712ddbc9f14631377f39a
Aziz Khoury <bentael@gmail.com>
2017-03-17 13:57:53 +0200

    1.0.0, upgrade to to-markdown 3 and decode entities

--- src/bbcode-to-markdown.js
+++ src/bbcode-to-markdown.js
@@ -12,67 +12,75 @@ var newBBCodeTags = require('./newBBCodeTags');
 var parser = new bbcodejs.Parser();
 
 newBBCodeTags.forEach(function(tag, i) {
-	parser.registerTag(tag.name, tag.klass);
+    parser.registerTag(tag.name, tag.klass);
 });
 
 var newToMarkdownOptions = require('./newToMarkdownOptions');
 
 (function(module) {
 
-	var brRegExp = /<br\s*[\/]?>/gmi;
-	var htmlMdjs = function (str) {
-		var window = jsdom.jsdom(null, null, {features: {FetchExternalResources: false}}).parentWindow;
+    var brRegExp = /<br\s*[\/]?>/gmi;
+    var htmlMdjs = function (str) {
+        var window = jsdom.jsdom(null, null, {features: {FetchExternalResources: false}}).parentWindow;
 
-		var newStr = (str || '').replace(brRegExp, '\n');
-		newStr = htmlMd(newStr, {window: window});
+        var newStr = (str || '').replace(brRegExp, '\n');
+        newStr = htmlMd(newStr, {window: window});
 
-		// Important! Prevents memory leaks. Thanks to @Fidelix
-		// https://github.com/akhoury/nodebb-plugin-import/issues/124
-		process.nextTick(function() {
-			window.close();
-		});
-		return newStr;
-	};
+        // Important! Prevents memory leaks. Thanks to @Fidelix
+        // https://github.com/akhoury/nodebb-plugin-import/issues/124
+        process.nextTick(function() {
+            window.close();
+        });
+        return newStr;
+    };
 
-	var urlRegExp = /\[url=\\"(.*)\\"\]/gi;
-	var convertBbcodeToHml = function (str, options) {
-		str = str.replace(urlRegExp, '[url=$1]');
-		str = parser.toHTML(str);
+    var urlRegExp = /\[url=\\"(.*)\\"\]/gi;
+    var convertBbcodeToHml = function (str, options) {
+        var opt = extend(true, {decode: true}, options);
 
-		if (!options || !options.noDecode) {
-			str = entities.decode(str);
-		}
-		return str;
-	};
+        str = str.replace(urlRegExp, '[url=$1]');
+        str = parser.toHTML(str);
 
-	var convertHtmlToMarkdown = function(str, options) {
-		var md = str;
-		try {
-			md = toMarkdown(str, extend(true, {}, newToMarkdownOptions, options));
-		} catch(e) {
-			if (options && options.fallback) {
-				// fallback to htmlMdjs if toMarkdown fails
-				md = htmlMdjs(str);
-			}
-		}
-		return md;
-	};
+        if (opt.decode) {
+            str = entities.decode(str);
+        }
+        return str;
+    };
 
-	var convertBbcodeToMarkdown = function(str, options) {
-		return convertHtmlToMarkdown(convertBbcodeToHml(str), options);
-	};
+    var convertHtmlToMarkdown = function(str, options) {
+        var md = str;
+        var opt = extend(true, {fallback: true, decode: true, gfm: true}, newToMarkdownOptions, options);
 
-	// backward compatible
-	var convert = convertBbcodeToMarkdown;
+        if (opt.decode) {
+            str = entities.decode(str);
+        }
 
-	// expose some more functions and aliases
-	convert.bbcodeToHTML = convertBbcodeToHml;
-	convert.bbcodeToMarkdown = convertBbcodeToMarkdown;
-	convert.convertHtmlToMarkdown = convertHtmlToMarkdown;
+        try {
+            md = toMarkdown(str, opt);
+        } catch(e) {
+            if (opt.fallback) {
+                // fallback to htmlMdjs if toMarkdown fails
+                md = htmlMdjs(str);
+            }
+        }
+        return md;
+    };
 
-	convert.htmlToMarkdown = toMarkdown;
-	convert.decodeEntities = entities.decode.bind(entities);
+    var convertBbcodeToMarkdown = function(str, options) {
+        return convertHtmlToMarkdown(convertBbcodeToHml(str), options);
+    };
 
-	module.exports = convert;
+    // backward compatible
+    var convert = convertBbcodeToMarkdown;
+
+    // expose some more functions and aliases
+    convert.bbcodeToHTML = convertBbcodeToHml;
+    convert.bbcodeToMarkdown = convertBbcodeToMarkdown;
+    convert.convertHtmlToMarkdown = convertHtmlToMarkdown;
+
+    convert.htmlToMarkdown = toMarkdown;
+    convert.decodeEntities = entities.decode.bind(entities);
+
+    module.exports = convert;
 
 })(module);
--- src/newToMarkdownOptions.js
+++ src/newToMarkdownOptions.js
@@ -1,43 +1,39 @@
 
-var extend = require('extend');
-
-var defaults = {
-	gfm: true
+module.exports = {
+    converters: [
+        {
+            filter: 'pre',
+            replacement: function (innerHTML) {
+                return '```\n' + innerHTML + '\n```';
+            }
+        },
+        {
+            filter: 'spoiler',
+            replacement: function (innerHTML) {
+                return '\n>! ' + (innerHTML || '').split(/\n[\n]+/).join('\n>! ');
+            }
+        },
+        {
+            filter: 'strike',
+            replacement: function (innerHTML) {
+                return '~~' + innerHTML + '~~';
+            }
+        },
+        {
+            filter: ['html', 'body', 'span', 'div', 'small', 'font']
+				.concat(
+					// weird tags i came across
+					['messagetemplate', 'zone', 'item', 'content']
+				),
+            replacement: function(innerHTML) {
+                return innerHTML;
+            }
+        },
+        {
+            filter: ['head', 'script', 'style', 'link', 'meta', 'input', 'textarea', 'button'],
+            replacement: function() {
+                return '';
+            }
+        }
+    ]
 };
-
-var options = extend(true, {
-	converters: [
-		{
-			filter: 'pre',
-			replacement: function (innerHTML) {
-				return '```\n' + innerHTML + '\n```';
-			}
-		},
-		{
-			filter: 'spoiler',
-			replacement: function (innerHTML) {
-				return '\n>! ' + (innerHTML || '').split(/\n[\n]+/).join('\n>! ');
-			}
-		},
-		{
-			filter: 'strike',
-			replacement: function (innerHTML) {
-				return '~~' + innerHTML + '~~';
-			}
-		},
-		{
-			filter: ['html', 'body', 'span', 'div', 'small', 'font', 'messagetemplate', 'zone', 'item', 'content'],
-			replacement: function(innerHTML) {
-				return innerHTML;
-			}
-		},
-		{
-			filter: ['head', 'script', 'style', 'link', 'meta', 'input', 'textarea', 'button'],
-			replacement: function() {
-				return '';
-			}
-		}
-	]
-}, defaults);
-
-module.exports = options;
